de8f3d9b99bb95935f2871e136cb83fe439ea5f5
Shimanta12 <sbemon1356@gmail.com>
2021-11-13 18:13:44 +0600

    edited firebase config file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,14 +1,3 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBX4u_bQaBV11MIWKFSa6i5amNUuZxRdsA",
-//     authDomain: "pedalpals-cce3b.firebaseapp.com",
-//     projectId: "pedalpals-cce3b",
-//     storageBucket: "pedalpals-cce3b.appspot.com",
-//     messagingSenderId: "513391929631",
-//     appId: "1:513391929631:web:9dd7a8dc3515dfe6c7e82a"
-// };
-
-// export default firebaseConfig;
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
